3b7837f8479404fea187664a99e259c743a77512
core <jinjs6@gmail.com>
2023-02-05 20:12:40 +0900

    refactor: @GetMapping의 url 이름을 /text/basic으로 변경

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/basic")
 public class BasicController {
 
-    @GetMapping("/text/baisc")
+    @GetMapping("/text/basic")
     public String textBasic(Model model) {
         model.addAttribute("data", "Hello Spring!");
         return "basic/text-basic";

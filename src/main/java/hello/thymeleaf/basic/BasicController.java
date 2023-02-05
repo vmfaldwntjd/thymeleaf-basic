d8be18db058f8f99fb5c9adda728b454adbb385c
core <jinjs6@gmail.com>
2023-02-05 21:39:36 +0900

    refactor: 모델값 변경

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -17,7 +17,7 @@ public class BasicController {
 
     @GetMapping("/text-unescaped")
     public String textUnescaped(Model model) {
-        model.addAttribute("data", "Hello Spring!");
+        model.addAttribute("data", "<b>Hello Spring!</b>");
         return "basic/text-unescaped";
     }
 }
